7ee48389999b9f178be11aa3fbba80ee40fbeaa8
ki52 <lesha.sinitsin@mail.ru>
2024-02-11 13:55:48 +0300

    save

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue';
+import './assets/main.css'
 
-import App from './app/App.vue';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app');
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
